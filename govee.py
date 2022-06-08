09b9ab0f2b0e3f5be4f5cdd2cc8fa12fbf8e92c1
riversteve <56084920+riversteve@users.noreply.github.com>
2022-06-08 01:51:38 -0400

    Added more actions. Looping menu for rapid testing

--- govee.py
+++ govee.py
@@ -2,6 +2,7 @@
 # https://govee-public.s3.amazonaws.com/developer-docs/GoveeDeveloperAPIReference.pdf
 # API v1.7
 
+from urllib.parse import urlencode
 from dotenv import load_dotenv
 import json
 import os
@@ -35,13 +36,16 @@ class Govee:
 def get_devices() -> list:
     resp = requests.get(API['get_devices'], headers={"Govee-API-Key": API['api_key']})
     resp.raise_for_status()
-    return resp.json()['data']['devices']
+    devices = resp.json()['data']['devices']
+    for device in devices:
+        device['state'] = get_device_state(device)
+    return devices
 
 def get_avail_controls(device: dict):
     print(device['supportCmds'])
 
 
-def power_device(device, cmd = "off"):
+def power_device(device, cmd = "off") -> None:
     headers = {
         "Govee-API-Key": API['api_key'],
         "Content-Type": "application/json",
@@ -50,25 +54,134 @@ def power_device(device, cmd = "off"):
         'device'    : device['device'],
         'model'     : device['model'],
         'cmd'       : {
-            "name": "turn",
-            "value": cmd
+            'name': 'turn',
+            'value': cmd
         }
     }
     resp = requests.put(API['put_device_control'], headers=headers, json=params)
     pass
 
+def set_color(device: dict, red: int, green: int, blue: int) -> None:
+    headers = {
+        "Govee-API-Key": API['api_key'],
+        "Content-Type": "application/json",
+    }
+    params = {
+        'device'    : device['device'],
+        'model'     : device['model'],
+        'cmd'       : {
+            'name': 'color',
+            'value': {
+                'r': red,
+                'g': green,
+                'b': blue
+            }
+        }
+    }
+    valid = [red, green, blue]
+    for n in valid:
+        if n < 0 or n > 255:
+            print("Invalid input. Colors must be between 0-255")
+            pass
+    resp = requests.put(API['put_device_control'], headers=headers, json=params)
+    pass
+
+def set_temperature(device: dict, temperature: int) -> None:
+    headers = {
+        "Govee-API-Key": API['api_key'],
+        "Content-Type": "application/json",
+    }
+    params = {
+        'device'    : device['device'],
+        'model'     : device['model'],
+        'cmd'       : {
+            'name': 'colorTem',
+            'value': temperature
+        }
+    }
+    if temperature < 2000 or temperature > 9000:
+        print("Invalid input. Color temperature must be between 2000-9000")
+        pass
+    resp = requests.put(API['put_device_control'], headers=headers, json=params)
+    pass
+
+def get_device_state(device) -> list:
+    headers = {
+        "Govee-API-Key": API['api_key'],
+        "Content-Type": "application/json",
+    }
+    req_params = {
+        'device'    : device['device'],
+        'model'     : device['model'],
+    }
+    resp = requests.get(url=API['get_device_state'] + urlencode(req_params), headers=headers, json=req_params)
+    resp.raise_for_status()
+    return resp.json()['data']['properties']
+
+def menu_print() -> None:
+    print("\n~ Govee API Controller ~")
+    print("1. Get devices")
+    print("2. Get device state")
+    print("3. Show device state")
+    print("4. Power ON")
+    print("5. Power OFF")
+    print("6. Set color")
+    print("7. Set color temperature")
+    print("0. Exit")
+
+def choose_colors() -> int:
+    try:
+        red =    int(input("Red     > "))
+        green =  int(input("Green   > "))
+        blue =   int(input("Blue    > "))
+    except:
+        print("Must be integers")
+        return 0, 0, 0
+    return red, green, blue
+
+def choose_temperature() -> int:
+    try:
+        temp =    int(input("Temperature (2000 - 9000) > "))
+    except:
+        print("Must be integers")
+        return 2000
+    return temp
 
 def main():
     # Get my devices
     devices = get_devices()
+    # Grab the first (and only) device
     light_stick = devices[0]
 
-    # Perform actions on devices
-    power_device(light_stick, "on")
-    time.sleep(5) # Wait 5 seconds
-    power_device(light_stick, "off")
-    
-
+    # Making a temporary menu for easier testing
+    while True:
+        menu_print()
+        choice = str(input(">>> "))
+        match choice:
+            case '0':
+                break
+            case '1':
+                devices = get_devices()
+                light_stick = devices[0]
+            case '2':
+                light_stick['state'] = get_device_state(light_stick)
+                print(light_stick['state'])
+            case '3': # Show device state
+                print(light_stick['state'])
+                get_avail_controls(light_stick)
+            case '4':
+                power_device(light_stick, "on")
+            case '5':
+                power_device(light_stick, "off")
+            case '6':
+                r, g, b = choose_colors()
+                set_color(light_stick, r, g, b)
+            case '7':
+                temp = choose_temperature()
+                set_temperature(light_stick, temp)
+            case other:
+                print("Invalid choice")
+            
 
 
 if __name__ == '__main__':
